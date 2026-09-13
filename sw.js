0e585503751d03753f03e6c849a4796ad962ccff
nagaitashouten-star <nagaita.shouten@gmail.com>
2026-09-13 09:03:30 +0900

    Refresh cache for cute collectible artwork

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='spell-garden-v4';
-const ASSETS=['/','/index.html','/styles-bundle.css','/app-loader.js','/stage3-fix.js','/stage4-fix.js','/style-parts/style-09.css','/manifest.webmanifest','/icon.svg'];
+const CACHE='spell-garden-v5';
+const ASSETS=['/','/index.html','/styles-bundle.css','/app-loader.js','/stage3-fix.js','/stage4-fix.js','/cute-items.js','/style-parts/style-09.css','/style-parts/style-10.css','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
